4ed2eaf5ea521e44cbc43617dec3b14ffab1d927
Steve Adams <steve@checkfront.com>
2018-08-16 00:21:55 -0700

    Add test to address the bug

--- tests/sum-two-largest-numbers.spec.ts
+++ tests/sum-two-largest-numbers.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-magic-numbers */
 
-import sumTwoLargestNumbers from '../src/sum-two-largest-numbers';
+import { sumTwoLargestNumbers } from '../src/sum-two-largest-numbers';
 
 it('Sums small arrays: [1, 1]', () => {
   expect(sumTwoLargestNumbers([1, 1])).toBe(2);
@@ -16,6 +16,10 @@ it('Sums negative and positive numbers properly: [0, -5, 5, 2, 5]', () => {
 
 it('Sums negative numbers properly: [-10, -5, -1, -3]', () => {
   expect(sumTwoLargestNumbers([-10, -5, -1, -3])).toBe(-4);
+});
+
+it('Sums properly when first number is smaller than second number: [90, 182, 139, 3, 6]', () => {
+  expect(sumTwoLargestNumbers([90, 182, 139, 3, 6])).toBe(321);
 });
 
 it('Fails to sum [5]', () => {
